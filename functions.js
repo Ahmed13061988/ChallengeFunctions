08dff2af0d9c4af2636bd8f0a02e4160389052b5
Ahmed Hussein <wolktheline@yahoo.com>
2022-09-17 23:10:30 -0600

    closure code challenge

--- functions.js
+++ functions.js
@@ -1,33 +1,41 @@
 "use strict";
 
-const poll = {
-  question: "What is your favourite programming language?",
-  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
-  // This generates [0, 0, 0, 0]. More in the next section! answers: new Array(4).fill(0),
-  answers: new Array(4).fill(0),
+// const poll = {
+//   question: "What is your favourite programming language?",
+//   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+//   // This generates [0, 0, 0, 0]. More in the next section! answers: new Array(4).fill(0),
+//   answers: new Array(4).fill(0),
 
-  registerNewAnswer() {
-    let input = Number(
-      prompt(
-        `${poll.question}\n${this.options.join("\n")}
-      (Write option number)`
-      )
-    );
-    input < this.answers.length &&
-      typeof input === "number" &&
-      this.answers[input]++;
-    this.displayResults();
-    this.displayResults("string");
-  },
-  displayResults(type = "array") {
-    if (type === "array") {
-      console.log(this.answers);
-    } else if (type === "string")
-      console.log(`Poll results are: ${this.answers.join(",")}`);
-  },
-};
-const btn = document.querySelector(".poll");
+//   registerNewAnswer() {
+//     let input = Number(
+//       prompt(
+//         `${poll.question}\n${this.options.join("\n")}
+//       (Write option number)`
+//       )
+//     );
+//     input < this.answers.length &&
+//       typeof input === "number" &&
+//       this.answers[input]++;
+//     this.displayResults();
+//     this.displayResults("string");
+//   },
+//   displayResults(type = "array") {
+//     if (type === "array") {
+//       console.log(this.answers);
+//     } else if (type === "string")
+//       console.log(`Poll results are: ${this.answers.join(",")}`);
+//   },
+// };
+// const btn = document.querySelector(".poll");
 
-btn.addEventListener("click", poll.registerNewAnswer.bind(poll));
+// btn.addEventListener("click", poll.registerNewAnswer.bind(poll));
 
-poll.displayResults.call({ answers: [5, 3, 2] });
+// poll.displayResults.call({ answers: [5, 3, 2] });
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+  document.querySelector("body").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
